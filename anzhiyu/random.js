4a632a5ffabbf5f87eabc978c1ee996bed5bcf69
Forwindreach <vince2363743898@163.com>
2024-08-18 14:41:58 +0800

    Site updated: 2024-08-18 14:41:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/17/hello-world/"];function toRandomPost(){
+var posts=["2024/08/18/欢迎你，刘恒源/","2024/08/18/虚拟机搭建/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
